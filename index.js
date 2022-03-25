dc95831bb45e2464fd01175f17cc0f97cbffaee3
woohyun0208 <102346294+woohyun0208@users.noreply.github.com>
2022-03-25 15:29:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let express = require('express');
-let app = express();
-let ejs = require('ejs');
-const haikus = require('./haikus.json');
-const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
-});
-
-app.listen(port);
+<!DOCTYPE>
+<html>
+<head>
+<title>나의페이지</title>
+</head>
+<body>
+<h1>login동아리</h1>
+<p>12345</p>
+</body>
+</html> 
